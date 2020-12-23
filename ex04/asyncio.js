e8ad9e12c56d96362f092753bd7f31c1d518a697
Toshiki　Nakamura <t.jack0624@gmail.com>
2020-12-23 22:20:06 +0900

    add ex04

--- ex04/asyncio.js
+++ ex04/asyncio.js
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   io.js                                              :+:      :+:    :+:   */
+/*   asyncio.js                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnakamur <tnakamur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/23 20:23:35 by tnakamur          #+#    #+#             */
-/*   Updated: 2020/12/23 22:19:22 by tnakamur         ###   ########.fr       */
+/*   Created: 2020/12/23 21:26:22 by tnakamur          #+#    #+#             */
+/*   Updated: 2020/12/23 22:19:03 by tnakamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const fs = require('fs');
-const { argv } = require('process');
 
-function read_file(arg)
-{
-	var buff = fs.readFileSync(arg, 'utf-8');
-	return buff.toString();
+var buff;
+var ans;
+
+function read_file(arg, call) {
+	buff = fs.readFileSync(arg, 'utf-8');
+	call(buff.toString());
 }
 
-var string = read_file(process.argv[2]);
-
-var cnt = 0;
-var i = 0;
-while (string[i])
-{
-	if (string[i] == '\n')
-		cnt++;
-	i++;
+var call_back = function (buf) {
+	ans = buf.split('\n').length - 1;
+	console.log(ans);
 }
-console.log(cnt);
+
+read_file(process.argv[2], call_back);
